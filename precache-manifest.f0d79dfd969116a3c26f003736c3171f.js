aff72b356e0938e1ffb2a97c61e2617d7c3a04e4
levy0601 <levy0601@gmail.com>
2020-07-18 04:20:51 -0700

    Updates

---
+++ precache-manifest.f0d79dfd969116a3c26f003736c3171f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91dacbf9388098426286af4fd4137917",
+    "url": "/Study-the-Vocabulary-List/index.html"
+  },
+  {
+    "revision": "37d5bcb326f6c96b837e",
+    "url": "/Study-the-Vocabulary-List/static/css/main.0cb9ff75.chunk.css"
+  },
+  {
+    "revision": "c34333340c99889c6c61",
+    "url": "/Study-the-Vocabulary-List/static/js/2.4d61776c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Study-the-Vocabulary-List/static/js/2.4d61776c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d5bcb326f6c96b837e",
+    "url": "/Study-the-Vocabulary-List/static/js/main.3fefdc2f.chunk.js"
+  },
+  {
+    "revision": "532acd873bba487aa6af",
+    "url": "/Study-the-Vocabulary-List/static/js/runtime-main.1170cdc8.js"
+  },
+  {
+    "revision": "414a23f5f5b82e15738a8b2175d35e4e",
+    "url": "/Study-the-Vocabulary-List/static/media/spinner.414a23f5.svg"
+  }
+]);
